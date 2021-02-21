adf7f46f02939dd9d7a7e7930de447bfed49e3a2
danst3in <50937807+danst3in@users.noreply.github.com>
2021-02-21 09:08:11 -0800

    useState custom forms hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { useForm } from "./useForm";
 
 /**
  * TODO:
@@ -9,11 +10,26 @@ import "./App.css";
  */
 
 function App() {
+  const [values, handleChange] = useForm({ email: "", password: "" });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} alt="logo-react" className="App-logo" />
         <p>hey!</p>
+        <p>A custom hook with useState is managing these two forms.</p>
+        <input
+          type="text"
+          name="email"
+          value={values.email}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={values.password}
+          onChange={handleChange}
+        />
       </header>
     </div>
   );
